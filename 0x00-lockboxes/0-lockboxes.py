45a25ae585c33c484d92e4154541c8c2c07f9f56
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-04-22 22:45:01 +0200

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+def canUnlockAll(boxes):
+    lb = []
+    b = len(boxes)
+    for i in boxes:
+        if len(i) == 0 and i is not boxes[b-1]:
+            return False
+        for j in i:
+            lb.append(j)
+    for index, keys in enumerate(boxes):
+        if index in lb or index < b-1:
+            return True
+        else:
+            return False
