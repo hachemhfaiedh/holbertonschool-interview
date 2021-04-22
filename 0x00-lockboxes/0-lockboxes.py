e4fc51f11ed72df9cdcc3123dcfee2ca57b7f0d1
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-04-22 23:12:58 +0200

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """method that determines if all the boxes can be opened"""
-    lb = []
-    b = len(boxes)
-    for i in boxes:
-        if len(i) == 0 and i is not boxes[b-1]:
-            return False
-        for j in i:
-            lb.append(j)
-    for index, keys in enumerate(boxes):
-        if index in lb or index < b-1:
-            return True
-        else:
-            return False
+    """
+    Method that determines if all the boxes can be opened
+    """
+    keys = [0]
+    for i, k in enumerate(boxes):
+        for j in k:
+            if j != i and j not in keys and j < len(boxes):
+                keys.append(j)
+    return len(keys) == len(boxes)
+    else:
+        return False
