d364ae9ec2df92f4cf9cdbc9b83ed10c853e527d
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-10-21 23:28:00 +0200

    N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-""" N queens """
+"""N queens"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
+def check_queen(p):
+    """N queens"""
+    for i in range(len(p) - 1):
+        d = p[i] - p[len(p) - 1]
+        if d == 0 or d * (-1) == len(p) - 1 - i or d == len(p) - 1 - i:
+            return False
+    return True
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+def n_queen(N, r, sol, p):
+    """N queens"""
+    if r == N:
+        print(sol)
     else:
-        yield a
+        for i in range(N):
+            p.append(i)
+            sol.append([r, i])
+            if check_queen(p):
+                n_queen(N, r + 1, sol, p)
+            p.pop()
+            sol.pop()
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+solution = []
+pos = []
+row = 0
+n_queen(n, row, solution, pos)
