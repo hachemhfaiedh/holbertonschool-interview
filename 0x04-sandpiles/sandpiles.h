bdb02ad7e2850dbbb1137eda708c62ed0b2f2986
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-10-27 21:27:40 +0200

    Sandpiles sum

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef _SANDPILES_
+#define _SANDPILES_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3]);
+int is_stable(int grid[3][3]);
+void copy_grid(int grid[3][3], int new_grid[3][3]);
+
+#endif
