6afde43aab39836e297e95b2db19912ab262c8d9
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-11-25 23:36:36 +0100

    betty

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -10,21 +10,21 @@
 */
 void heapify(int *array, size_t size, size_t calcsize, int idx)
 {
- int root = idx;
- int left = 2 * idx + 1;
- int right = 2 * idx + 2;
- int n = (int)size;
+    int root = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    int n = (int)size;
 
- if (left < n && array[left] > array[root])
-  root = left;
- if (right < n && array[right] > array[root])
-  root = right;
- if (root != idx)
- {
-  swap(array[idx], array[root]);
-  print_array(array, calcsize);
-  heapify(array, n, calcsize, root);
- }
+    if (left < n && array[left] > array[root])
+    root = left;
+    if (right < n && array[right] > array[root])
+    root = right;
+    if (root != idx)
+    {
+        swap(array[idx], array[root]);
+        print_array(array, calcsize);
+        heapify(array, n, calcsize, root);
+    }
 }
 
 /**
@@ -35,13 +35,13 @@ void heapify(int *array, size_t size, size_t calcsize, int idx)
 */
 void heap_sort(int *array, size_t size)
 {
- int i;
- for (i = size / 2 - 1; i >= 0; i--)
-  heapify(array, size, size, i);
- for (i = size - 1; i > 0; i--)
- {
-  swap(array[0], array[i]);
-  print_array(array, size);
-  heapify(array, i, size, 0);
- }
+    int i;
+    for (i = size / 2 - 1; i >= 0; i--)
+    heapify(array, size, size, i);
+    for (i = size - 1; i > 0; i--)
+    {
+        swap(array[0], array[i]);
+        print_array(array, size);
+        heapify(array, i, size, 0);
+    }
 }
